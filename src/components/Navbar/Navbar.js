2a1068160d642dfc2a337e37f27e5a15e1654796
BirrellC <vaiezmedia@gmail.com>
2022-02-26 15:07:01 +0000

    Feat: added navbar and js functionality

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import './NavbarStyles.css';
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav);
+    console.log(nav);
+  };
+
   return (
     <div className='navbar'>
       <div className='container'>
-        <div className='logo'></div>
+        <div className='logo'>
+          <h1>Sunnyside</h1>
+        </div>
+
+        <ul className={nav ? 'nav-menu active' : 'nav-menu'}>
+          <li>About</li>
+          <li>Services</li>
+          <li>Projects</li>
+          <li>Contacts</li>
+        </ul>
+        <div onClick={handleNav} className='hamburger'>
+          {!nav ? <FaBars className='icon' /> : <FaTimes className='icon' />}
+        </div>
       </div>
     </div>
   );
