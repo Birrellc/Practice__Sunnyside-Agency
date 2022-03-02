de90e3afacb474282e0d7a3dc9c9fe232f8c7ef7
BirrellC <vaiezmedia@gmail.com>
2022-03-02 15:36:27 +0000

    Feat/style: made gallery responsive

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -5,11 +5,27 @@ const Gallery = () => {
   return (
     <div className='gallery'>
       <div className='container'>
-        <div className='content'>
-          <img src={require('./images/image-gallery-milkbottles.jpg')} alt='' />
-          <img src={require('./images/image-gallery-orange.jpg')} alt='' />
-          <img src={require('./images/image-gallery-cone.jpg')} alt='' />
-          <img src={require('./images/image-gallery-sugar-cubes.jpg')} alt='' />
+        <div className='gallery-content'>
+          <img
+            className=''
+            src={require('./images/image-gallery-milkbottles.jpg')}
+            alt=''
+          />
+          <img
+            className=''
+            src={require('./images/image-gallery-orange.jpg')}
+            alt=''
+          />
+          <img
+            className=''
+            src={require('./images/image-gallery-cone.jpg')}
+            alt=''
+          />
+          <img
+            className=''
+            src={require('./images/image-gallery-sugar-cubes.jpg')}
+            alt=''
+          />
         </div>
       </div>
     </div>
